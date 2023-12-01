40b4fe4ddf466ff3382984a235f7a2943d1fbe0a
Guido Pontet <gpontet@fu.do>
2023-12-01 20:32:18 -0300

    feat: add components structure

---
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-slow',
+  standalone: true,
+  imports: [],
+  template: ``,
+  styles: ``
+})
+export class HeavyLoadersSlowComponent {
+
+}
