e2ce97635f406a092eb6c956b7f694d232a87e9c
Guido Pontet <gpontet@fu.do>
2023-12-01 20:48:45 -0300

    feat: add routes configuration

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule ],
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {
+export default class DashboardComponent {
 
 }
