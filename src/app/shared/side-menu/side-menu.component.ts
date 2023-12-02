b6bffdd295b543996961b28f77e6443720210ee4
Guido Pontet <gpontet@fu.do>
2023-12-01 21:54:23 -0300

    feat: display menu items

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './side-menu.component.html',
   styles: ``
 })
