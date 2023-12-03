b1653ed65efa69dd26b30c437266f38b314e6f27
Guido Pontet <gpontet@fu.do>
2023-12-03 20:44:31 -0300

    feat: add defer blocks

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeavyLoadersSlowComponent],
   templateUrl: './defer-views.component.html',
   styles: ``
 })
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
-  template: ``,
-  styles: ``
+  imports: [CommonModule],
+  template: `
+    <section [ngClass]="['w-full h-[300px]', cssClass]">
+      heavy-loaders-slow works!
+    </section>
+  `,
 })
 export class HeavyLoadersSlowComponent {
+  @Input({ required: true }) cssClass!: string;
 
+  constructor() {
+    const start = Date.now();
+
+    // Simulate heavy load and block the UI thread
+    while (Date.now() - start < 3000) {
+      // Heavy load
+    }
+  }
 }
