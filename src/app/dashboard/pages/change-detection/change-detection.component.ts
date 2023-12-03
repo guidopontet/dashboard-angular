c7a9e0a7d09977496988869361442bddc8a3f6e3
Guido Pontet <gpontet@fu.do>
2023-12-03 20:22:46 -0300

    feat: add change-detection section

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ CommonModule, TitleComponent ],
   templateUrl: './change-detection.component.html',
   styles: ``
 })
 export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change Detection: ${this.frameworkAsSignal().name}`
+  );
 
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: '2023-04-01',
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: '2023-04-01',
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // This will not work unless we use Default change detection strategy, which is less performant
+      // this.frameworkAsProperty.name = 'React';
+
+      this.frameworkAsSignal.update(framework => ({
+        ...framework,
+        name: 'React',
+      }));
+
+      console.log('Done');
+    }, 2000);
+  }
 }
